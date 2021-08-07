1345d485d50ab87283fc38407458a8555291191c
Veysel Akbalik <veysel.akbalik97@gmail.com>
2021-08-08 00:38:00 +0300

    Firebase Entegre Edildi / Login Page Olusturuldu

--- app/src/main/java/com/veyselakbalik/btkfirebasefotoapp/MainActivity.kt
+++ app/src/main/java/com/veyselakbalik/btkfirebasefotoapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.veyselakbalik.btkfirebasefotoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun girisYap(view : View){
+
+    }
+    fun kayitOl(view:View){
+
     }
 }
